bd4c38d684d201bf0b24cdadcea389f964c28cff
Taylor Peat <taylorpeat@hotmail.com>
2015-11-14 02:07:23 -0500

    Updates as per TA comments

--- main.rb
+++ main.rb
@@ -6,19 +6,6 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => '98h2jowbvu05jd3'
 
 helpers do
-  SUITS = [:S, :C, :D, :H]
-  VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-
-  def create_deck
-    SUITS.product(VALUES).shuffle!
-  end
-
-  def deal_cards
-    2.times do
-      session[:player_cards] << session[:deck].pop
-      session[:dealer_cards] << session[:deck].pop
-    end
-  end
 
   def calculate_total(cards)
     cards = hide_dealer_card(cards)
@@ -29,60 +16,6 @@ helpers do
       total += 1 if card[1] == 'A'
     end
     total = correct_for_aces(cards, total)
-  end
-
-  def hide_dealer_card(cards)
-    if cards == session[:dealer_cards] && !session[:show_dealer_card]
-      cards = [cards[0]]
-    end
-    cards
-  end
-
-  def correct_for_aces(cards, total)
-    cards.each do |card|
-      if card[1] == 'A' && total > 21
-        total -= 10
-      end
-    end
-    total
-  end
-
-
-  def card_image(card, idx, hand_size)
-    suit = determine_suit(card[0])
-    value = determine_card_value(card[1])
-    card_class = determine_card_class(idx, hand_size)
-    
-    "<img src='/images/cards/#{suit}_#{value}.jpg' class='#{card_class}'>"
-  end
-
-  def determine_suit(suit)
-    case suit
-    when :S then "spades"
-    when :C then "clubs"
-    when :D then "diamonds"
-    when :H then "hearts"
-    end
-  end
-
-  def determine_card_value(value)
-    if ['J', 'Q', 'K', 'A'].include?(value)
-      new_value = case value
-      when 'J' then "jack"
-      when 'Q' then "queen"
-      when 'K' then "king"
-      when 'A' then "ace"
-      end
-    end
-    new_value ||= value
-  end
-
-  def determine_card_class(idx, hand_size)
-    if hand_size == idx + 1 || idx == 0
-      "end_card"
-    else
-      "middle_card"
-    end
   end
 
   def determine_results
@@ -106,6 +39,74 @@ helpers do
       session[:balance] += session[:bet]
     end
     result + "</br>Your new balance is $#{session[:balance]}.</div>"
+  end
+end
+
+SUITS = [:S, :C, :D, :H]
+VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+def create_deck
+  SUITS.product(VALUES).shuffle!
+end
+
+def deal_cards
+  2.times do
+    session[:player_cards] << session[:deck].pop
+    session[:dealer_cards] << session[:deck].pop
+  end
+end
+
+def hide_dealer_card(cards)
+  if cards == session[:dealer_cards] && !session[:show_dealer_card]
+    cards = [cards[0]]
+  end
+  cards
+end
+
+def correct_for_aces(cards, total)
+  cards.each do |card|
+    if card[1] == 'A' && total > 21
+      total -= 10
+    end
+  end
+  total
+end
+
+
+def card_image(card, idx, hand_size)
+  suit = determine_suit(card[0])
+  value = determine_card_value(card[1])
+  card_class = determine_card_class(idx, hand_size)
+  
+  "<img src='/images/cards/#{suit}_#{value}.jpg' class='#{card_class}'>"
+end
+
+def determine_suit(suit)
+  case suit
+  when :S then "spades"
+  when :C then "clubs"
+  when :D then "diamonds"
+  when :H then "hearts"
+  end
+end
+
+def determine_card_value(value)
+  if ['J', 'Q', 'K', 'A'].include?(value)
+    new_value = case value
+                when 'J' then "jack"
+                when 'Q' then "queen"
+                when 'K' then "king"
+                when 'A' then "ace"
+                end
+  end 
+  new_value || value
+end
+
+def determine_card_class(idx, hand_size)
+  if hand_size == idx + 1 || idx == 0
+    "end_card"
+  else
+    "middle_card"
   end
 end
 
@@ -170,9 +171,6 @@ get '/game' do
 end
 
 post '/game/player/hit' do
-  @slide = true
-  erb :game
-  @slide = false
   session[:player_cards] << session[:deck].pop
   if calculate_total(session[:player_cards]) >= 21
     session[:stand] = true
@@ -197,10 +195,6 @@ post '/game/player/stand' do
   redirect '/game/update'
 end
 
-# get '/game/dealer' do
-#   erb :game, :layout => false
-# end
-
 post '/game/dealer/hit' do
   session[:dealer_cards] << session[:deck].pop
   if calculate_total(session[:dealer_cards]) < 17
@@ -210,10 +204,6 @@ post '/game/dealer/hit' do
   end
   redirect '/game/update'
 end
-
-# get '/game/dealer/hit' do
-#   erb :game, :layout => false
-# end
 
 get '/quit' do
   @balance = session[:balance]
